52cec035c5fb9b374128e24d392f3f978ea2c554
Noa Kim <kimnoa0513@gmail.com>
2025-08-16 16:07:16 +0900

    API fix

--- app/api/music.js
+++ app/api/music.js
@@ -13,25 +13,34 @@ document.addEventListener("DOMContentLoaded", () => {
     updateTheme();
     prefersDarkScheme.addEventListener("change", updateTheme);
 
-    fetch("https://simple-proxy.taein.workers.dev/?destination=https://yuntae.in/api/music/recent/noa")
-        .then(res => res.json())
-        .then(data => {
-            var artistName = data.data[0].attributes.artistName;
-            var songName = data.data[0].attributes.name;
-            var albumCover = data.data[0].attributes.artwork.url;
+    const apiUrl = "https://yuntae.in/api/music/recent";
+    const userToken = "AuX+Y68jIJ4ojOSi9EpcKz9828+u2CwX1JDxKVnly4TkTKoz8SaIvrQA5rmLHdqVc4wVgMSLGEuqnhKZ0FAcldthmXplETm8tNFATc4V+12u3szBQcCvV9yhtLA/DZMLupw9kFTdlVQAk31dM3yi7myTWy0Yn2RUluSGjGGdsxahpKMscuc7Pmow4PN4qbCc5sG/E9WLL2ZwMjVairmY7CEDP1p+v1kWkHnnMVxSql38OI6xzw==";
 
-            var resizeCover = albumCover.replace("{w}x{h}", "1000x1000");
 
-            document.getElementById("artist").innerHTML = artistName;
-            document.getElementById("song").innerHTML = songName;
-
-            // --- 여기만 수정됩니다 ---
-            // ID가 'album-art'인 img 태그를 찾아 src 속성을 설정합니다.
-            document.getElementById("album-art").src = resizeCover;
-            // -------------------------
-
+        fetch(apiUrl,{
+            headers: {
+                'Content-Type': 'application/json',
+                'user-token': userToken
+            }
         })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+            .then(res => res.json())
+            .then(data => {
+                var artistName = data.data[0].attributes.artistName;
+                var songName = data.data[0].attributes.name;
+                var albumCover = data.data[0].attributes.artwork.url;
+
+                var resizeCover = albumCover.replace("{w}x{h}", "1000x1000");
+
+                document.getElementById("artist").innerHTML = artistName;
+                document.getElementById("song").innerHTML = songName;
+
+                // --- 여기만 수정됩니다 ---
+                // ID가 'album-art'인 img 태그를 찾아 src 속성을 설정합니다.
+                document.getElementById("album-art").src = resizeCover;
+                // -------------------------
+
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
 });
